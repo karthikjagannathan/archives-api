ae8ee2636bd084a64207d2abb19abaf1c1dd0869
Karthik J <karthik@codinglife.net>
2019-09-10 21:18:33 -0500

    added db, catalog controller, removed dist folder

--- dist/controllers/health.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import * as utils from '../helpers/util';
-// const logger = utils.getLogger(__filename);
-const getHealth = (req, res) => {
-    //logger.info()
-    res.status(200).send('Server is up!');
-};
-exports.getHealth = getHealth;
