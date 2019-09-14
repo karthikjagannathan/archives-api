b3d480cee8eefe56dba1dcf442300c15d2a16ae1
Karthik J <karthik@codinglife.net>
2019-09-13 23:24:15 -0500

    custom error handling

--- src/controllers/catalogs.controller.ts
+++ src/controllers/catalogs.controller.ts
@@ -3,8 +3,9 @@
  *
  */
 
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
+import HttpException from '../exceptions/http.exceptions';
 // import { getDb } from '../db';
 import * as utils from '../helpers/util.helper';
 import { Catalog } from '../db/models/catalog.model';
@@ -12,20 +13,18 @@ import { Book } from '../db/models/book.model';
 
 const logger = utils.getLogger(__filename);
 
-const getCatalogs = async (req: Request, res: Response): Promise<void> => {
+const getCatalogs = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   logger.info('getCatalogs');
 
   try {
     res.status(200).send(await Catalog.find());
   } catch (e) {
     logger.debug(e);
-    res.status(400).send({
-      error: e.message,
-    });
+    next(new HttpException(400, 'missing or invalid parameters'));
   }
 };
 
-const getCatalogDetails = async (req: Request, res: Response): Promise<void> => {
+const getCatalogDetails = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   logger.info(`getCatalogDetails for ${req.params.catId}`);
 
   const catId = req.params.catId;
@@ -34,24 +33,24 @@ const getCatalogDetails = async (req: Request, res: Response): Promise<void> => 
     res.status(200).send(await Catalog.findById(catId));
   } catch (e) {
     logger.debug(e);
-    res.status(400).send({
-      error: e.message,
-    });
+    next(new HttpException(400, 'missing or invalid parameters'));
   }
 };
 
-const getCatalogBooks = async (req: Request, res: Response): Promise<void> => {
+const getCatalogBooks = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   logger.info(`getCatalogBooks for ${req.params.catId}`);
 
-  const catId = req.params.catId;
-
   try {
-    res.status(200).send(await Book.find({ catalogId: catId }));
+    const books = await Book.find({ catalogId: req.params.catId });
+
+    if (books.length) {
+      res.status(200).send(books);
+    } else {
+      next(new HttpException(404, 'books not found'));
+    }
   } catch (e) {
     logger.debug(e);
-    res.status(400).send({
-      error: e.message,
-    });
+    next(new HttpException(400, 'missing or invalid parameters'));
   }
 };
 
--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -5,6 +5,7 @@
 
 import { Request, Response } from 'express';
 import HttpException from '../exceptions/http.exceptions';
+import { NextFunction } from 'connect';
 
 const fofHandler = (_request: Request, response: Response): void => {
   const status = 404;
@@ -15,13 +16,14 @@ const fofHandler = (_request: Request, response: Response): void => {
   });
 };
 
-const errorHandler = (error: HttpException, _request: Request, response: Response): void => {
+const errorHandler = (error: HttpException, _request: Request, response: Response, next: NextFunction): void => {
   const status = error.status || 500;
   const message = error.message || 'Internal server error';
   response.status(status).send({
     status,
     message,
   });
+  next();
 };
 
 export { fofHandler, errorHandler };
